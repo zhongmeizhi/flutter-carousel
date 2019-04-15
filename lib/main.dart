e3367fae7e4378f0206f4f446bf01d50b9bb4442
zhongmeizhi <330447171@qq.com>
2019-04-15 16:45:35 +0800

    自动轮播

--- lib/main.dart
+++ lib/main.dart
@@ -22,29 +22,6 @@ class MyHomePage extends StatelessWidget {
   // mock 屏幕宽度
   final double _screenWidth = 414.0;
 
-  // mock数据
-  // 需展现的轮播Widget列表
-  final List carouselList = <Widget>[
-    Container(
-      color: Colors.deepOrange,
-      child: Center(
-        child: Text('111'),
-      ),
-    ),
-    Container(
-      color: Colors.amberAccent,
-      child: Center(
-        child: Text('222'),
-      ),
-    ),
-    Container(
-      color: Colors.pink,
-      child: Center(
-        child: Text('333'),
-      ),
-    )
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,6 +38,15 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-
-
-
+// mock 轮播列表
+List carouselList = <Widget>[
+    Container(
+      color: Colors.deepOrange, child: Center(child: Text('111'))
+    ),
+    Container(
+      color: Colors.amberAccent, child: Center(child: Text('222'))
+    ),
+    Container(
+      color: Colors.pink, child: Center(child: Text('333'))
+    )
+  ];
